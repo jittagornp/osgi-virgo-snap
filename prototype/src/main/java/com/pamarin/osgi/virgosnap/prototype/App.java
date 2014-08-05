99b6cdd947079a7d9f41bbe520f7050c151354cc
jittagorn pitakmetagoon <jittagornp@gmail.com>
2014-08-05 17:13:23 +0700

    initial commit

---
+++ prototype/src/main/java/com/pamarin/osgi/virgosnap/prototype/App.java
@@ -0,0 +1,13 @@
+package com.pamarin.osgi.virgosnap.prototype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
